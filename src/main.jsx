6608c5daeb79be57b89fd5052ebeeebbb83165f4
Connor Hayes <conmhayes@gmail.com>
2024-01-18 12:18:54 +0000

    changes to app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter as Router } from "react-router-dom";
+import { Projects } from './pages/index.jsx';
+// import { HashRouter as Router } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/Portfolio/",
+    element: <App/>,
+    children: [
+      {
+        path: "/Portfolio/",
+        element: <Projects/>
+      },
+      {
+        path: "/Portfolio/",
+        element: <Projects/>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
