ebb1fd3112d1a87e5af29dfaeada5af861903f11
Connor Hayes <conmhayes@gmail.com>
2024-01-18 17:30:31 +0000

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,10 +10,12 @@ function Navbar() {
       <nav>
         <h1>Connor Hayes</h1>
         <ul className="nav-links">
-          <NavLink to='/' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Projects</NavLink>
-          <NavLink to='/About' style={({ isActive }) => (isActive ? activeStyle : undefined)}>About Me</NavLink>
-          <NavLink to='/Education' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Education</NavLink>
-          <NavLink to='/Skills' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Skills</NavLink>
+          <div className='nav-list'>
+            <NavLink to='/' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Projects</NavLink>
+            <NavLink to='/About' style={({ isActive }) => (isActive ? activeStyle : undefined)}>About Me</NavLink>
+            <NavLink to='/Education' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Education</NavLink>
+            <NavLink to='/Skills' style={({ isActive }) => (isActive ? activeStyle : undefined)}>Skills</NavLink>
+          </div>
         </ul>
       </nav>
       <Outlet />
--- src/pages/EducationPage/index.jsx
+++ src/pages/EducationPage/index.jsx
@@ -4,16 +4,16 @@ function Educations() {
         <>
         <h1>Educations</h1>
 
-        <h3>2020-2023</h3>
+        <h4>2020-2023</h4>
         <h2>University Of Hertfordshire</h2>
         <p>Bsc in Physics</p>
 
-        <h3>2018-2020</h3>
+        <h4>2018-2020</h4>
         <h2>Tendering Technology College Sixth Form</h2>
         <p>Maths, Physics, Chemistry</p>
         <p>B, C, C</p>
 
-        <h3>2013-2018</h3>
+        <h4>2013-2018</h4>
         <h2>Tendering Technology College Secondary School</h2>
         <p>11 GCSEs</p>
         <p>A*-C & 7-5</p>
