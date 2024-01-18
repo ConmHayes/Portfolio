407f98ac42c4fa6f8a4bc8cb8317ac29d7a57340
Connor Hayes <conmhayes@gmail.com>
2024-01-18 12:24:52 +0000

    render site now

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Projects } from './pages/index.jsx';
-// import { HashRouter as Router } from "react-router-dom";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: "/Portfolio/",
-    element: <App/>,
-    children: [
-      {
-        path: "/Portfolio/",
-        element: <Projects/>
-      },
-      {
-        path: "/Portfolio/",
-        element: <Projects/>
-      },
-    ]
-  }
-])
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router = {router}/>
+      <Router>
+        <App />
+      </Router>
   </React.StrictMode>,
 )
