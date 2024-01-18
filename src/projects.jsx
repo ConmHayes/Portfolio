814c3f39c28a52823bc2f0c14e0825100e53f322
Connor Hayes <conmhayes@gmail.com>
2024-01-18 12:28:30 +0000

    fixed public folder

--- src/projects.jsx
+++ src/projects.jsx
@@ -2,22 +2,22 @@ const descriptions = [
     {
         name: 'Educational Game',
         description: 'An educational quiz site using HTML, CSS, JavaScript.',
-        img: '/public/Project_1.jpg',
+        img: '/Project_1.jpg',
     },
     {
         name: 'Library Service',
         description: 'A Liberary service site using HTML, CSS, JavaScript and an PostgreSQL.',
-        img: '/public/Project_2.jpg',
+        img: '/Project_2.jpg',
     },
     {
         name: 'Time Table and Note Taking',
         description: 'A site to timetable and take notes using React',
-        img: '/public/Project_3.jpg',
+        img: '/Project_3.jpg',
     },
     {
         name: 'Book/Game Sharing',
         description: 'A Book/Game sharing site using React and Flask',
-        img: '/public/Project_4.jpg',
+        img: '/Project_4.jpg',
     }
 ]
 
